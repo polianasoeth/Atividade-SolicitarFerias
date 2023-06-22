608ca8bd2991a8b47ff7643c3c150e5cd906b611
Poliana Soeth <polianacristinasoeth16@gmail.com>
2023-06-22 09:01:38 -0300

    Ajustes

---
+++ infos.js
@@ -0,0 +1,7 @@
+function h() {
+    var nomeColaborador = document.getElementById('campo-nome-colaborador').value
+    var dataInicio = document.getElementById('campo-data-inicio').value
+    var dataFim = document.getElementById('campo-data-fim').value
+    
+
+}
