80f96dc044321647e500066c12ce719575a86db4
Poliana Soeth <polianacristinasoeth16@gmail.com>
2023-06-23 19:00:20 -0300

    Atualização total férias

--- vacancy-form.js
+++ vacancy-form.js
@@ -2,38 +2,24 @@ class VacancyForm extends HTMLElement {
     constructor() {
         super();
         const template = document.createElement('template')
-        template.innerHTML = `<form class="vacancy-form">
-    
-        <h1><strong>Solicitação de Férias</strong></h1>
-    
-        <div class="input-area">
-            <label class="employee-name-label" for="Nome Colaborador"><strong>Nome do colaborador</strong></label><input id="campo-nome-colaborador" type="text" placeholder="Nome completo" />
-            <span id="error-nome" class="texto-erro" hidden >Nome é obrigatório</span>
-        </div>
-    
-        <div class="flex-container"> 
-    
-            <div class="flex-item"><label for="Data Inicio"><strong>Data de início</strong></label><input id="campo-data-inicio" type="date" placeholder="dd-mm-aaaa" /></div> 
-            <span id="error-data-inicio" class="data-erro" hidden >Data de inicio é obrigatória!</span>
-    
-            <div class="flex-item"><label for="Data Termino"><strong>Data de término</strong></label><input id="campo-data-termino" type="date" placeholder="dd-mm-aaaa" /></div> 
-            <span id="error-data-termino" class="data-erro" hidden >Data do término é obrigatória!</span>
-    
-        </div>
-    
-                  
-        <button class="styled" type="button" onclick="solicitarFerias()">Gerar Solicitação</button>
-    
-    </form>    `
+        fetch('vacancy-form.html')
+            .then(res => res.text())
+            .then(html => {
+                template.innerHTML = html
+                this.shadowRoot.appendChild(template.content.cloneNode(true))
+                this.shadowRoot.querySelector('button').onclick = this.solicitarFerias
+            })
         this.attachShadow({ mode: 'open' })
-        this.shadowRoot.appendChild(template.content.cloneNode(true))
+
     }
 
     solicitarFerias() {
-        const employeeName = document.getElementById('campo-nome-colaborador').value
-        const startDate = document.getElementById('campo-data-inicio').value
-        const endDate = document.getElementById('campo-data-termino').value
-        this.shadowRoot.innerHTML = `<vacancy-message employeeName="${employeeName}" startDate="${startDate}" endDate="${endDate}"></vacancy-message>`
+        const vacancyForm = document.querySelector('vacancy-form');
+        const vacancyFormShadowRoot = vacancyForm && vacancyForm.shadowRoot;
+        const employeeName = vacancyFormShadowRoot.getElementById('campo-nome-colaborador').value
+        const startDate = vacancyFormShadowRoot.getElementById('campo-data-inicio').value
+        const endDate = vacancyFormShadowRoot.getElementById('campo-data-termino').value
+        vacancyFormShadowRoot.innerHTML = `<vacancy-message employeeName="${employeeName}" startDate="${startDate}" endDate="${endDate}"></vacancy-message>`
     }
 }
 customElements.define('vacancy-form', VacancyForm)
--- vacancy-message.js
+++ vacancy-message.js
@@ -5,10 +5,30 @@ class VacancyMessage extends HTMLElement {
     }
 
     connectedCallback() {
-        const employeeName = this.getAttribute('employeeName')
-        const startDate = this.getAttribute('startDate')
-        const endDate = this.getAttribute('endDate')
-        const todayDate = new Date().toLocaleDateString()
+        fetch('vacancy-message.html')
+            .then(res => res.text())
+            .then(html => {
+                this.shadowRoot.innerHTML = html
+                const employeeName = this.getAttribute('employeeName')
+                const startDate = this.getAttribute('startDate')
+                const endDate = this.getAttribute('endDate')
+                const todayDate = new Date().toLocaleDateString()
+                const employeeNameStrong = this.shadowRoot.getElementById('employeeName')
+                const startDateStrong = this.shadowRoot.getElementById('startDate')
+                const endDateStrong = this.shadowRoot.getElementById('endDate')
+                const attStrong = this.shadowRoot.getElementById('employeeName')
+                const todayDateStrong = this.shadowRoot.getElementById('todayDate')
+                employeeNameStrong.innerText=employeeName
+                startDateStrong.innerText=startDate
+                endDateStrong.innerText=endDate
+                attStrong.innerText=employeeName
+                todayDateStrong.innerText=todayDate
+                
+
+               
+                this.shadowRoot.querySelector('button').onclick = this.goToVacancyForm
+            })
+
         const vacancyMessage = `Caro(a) responsável 
         Gostaria de solicitar minhas férias: 
         Nome do Colaborador: ${employeeName} 
@@ -18,7 +38,14 @@ class VacancyMessage extends HTMLElement {
         Atenciosamente, ${employeeName}
         Gerado em ${todayDate}
         `
-        this.shadowRoot.innerHTML = vacancyMessage
+        
+    }
+
+    goToVacancyForm() {
+        const vacancyMessage = document.querySelector('vacancy-message');
+        const vacancyMessageShadowRoot = vacancyMessage && vacancyMessage.shadowRoot;
+        vacancyMessageShadowRoot.innerHTML = `<vacancy-form></vacancy-form>`
+
     }
 }
 customElements.define('vacancy-message', VacancyMessage)
