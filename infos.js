009cbce483d32c12d533acd5927a70673dad6e73
Poliana Soeth <polianacristinasoeth16@gmail.com>
2023-06-23 14:23:42 -0300

    Implementação html e css

--- infos.js
+++ infos.js
@@ -1,7 +1,25 @@
 function h() {
-    var nomeColaborador = document.getElementById('campo-nome-colaborador').value
-    var dataInicio = document.getElementById('campo-data-inicio').value
-    var dataFim = document.getElementById('campo-data-fim').value
-    
+    var dataHoje = new Date().toLocaleDateString()
+
+var nomeFuncionario = ("Digite o nome do funcionário")
+var dataInicio = ("Digite a data de início das férias")
+var dataFim = ("Digite a data de fim das férias")
+
+
+// Forma com concatenação
+var resultado = "Caro(a) responsável  \n  Gostaria de solicitar minhas férias:  \n Nome do funcionário: " + nomeFuncionario 
+
+var cartaFerias = `Caro(a) responsável 
+Gostaria de solicitar minhas férias: 
+Nome do funcionário: ${nomeFuncionario} 
+Data de inicio: ${dataInicio}
+Data de fim: ${dataFim}
+Agradeço a atenção
+Atenciosamente, ${nomeFuncionario}
+Gerado em ${dataHoje}
+`
+
+console.log(cartaFerias)
+
 
 }
