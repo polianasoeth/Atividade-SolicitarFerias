b7cfd45b4c896abd50f22ffc9f49264c802897a7
Poliana Soeth <polianacristinasoeth16@gmail.com>
2023-06-25 21:17:58 -0300

    Adicionando ID diferente na mensagem de atenciosamente e corrigindo invalidação de elementos no DOM

--- vacancy-form.js
+++ vacancy-form.js
@@ -17,9 +17,15 @@ class VacancyForm extends HTMLElement {
         const vacancyForm = document.querySelector('vacancy-form');
         const vacancyFormShadowRoot = vacancyForm && vacancyForm.shadowRoot;
         const employeeName = vacancyFormShadowRoot.getElementById('campo-nome-colaborador').value
-        const startDate = vacancyFormShadowRoot.getElementById('campo-data-inicio').value
-        const endDate = vacancyFormShadowRoot.getElementById('campo-data-termino').value
-        vacancyFormShadowRoot.innerHTML = `<vacancy-message employeeName="${employeeName}" startDate="${startDate}" endDate="${endDate}"></vacancy-message>`
+        const startDate = new Date(vacancyFormShadowRoot.getElementById('campo-data-inicio').value).toLocaleDateString()
+        const endDate = new Date(vacancyFormShadowRoot.getElementById('campo-data-termino').value).toLocaleDateString()
+        const parentElement = vacancyForm.parentElement
+        parentElement.removeChild(vacancyForm)
+        const vacancyMessage = document.createElement('vacancy-message')
+        vacancyMessage.setAttribute('employeeName', employeeName)
+        vacancyMessage.setAttribute('startDate', startDate)
+        vacancyMessage.setAttribute('endDate', endDate)
+        parentElement.appendChild(vacancyMessage)
     }
 }
 customElements.define('vacancy-form', VacancyForm)
--- vacancy-message.js
+++ vacancy-message.js
@@ -12,39 +12,31 @@ class VacancyMessage extends HTMLElement {
                 const employeeName = this.getAttribute('employeeName')
                 const startDate = this.getAttribute('startDate')
                 const endDate = this.getAttribute('endDate')
-                const todayDate = new Date().toLocaleDateString()
+                const todayDate = new Date().toLocaleDateString('pt-BR', { day: '2-digit', month: 'long', year: 'numeric'})
                 const employeeNameStrong = this.shadowRoot.getElementById('employeeName')
                 const startDateStrong = this.shadowRoot.getElementById('startDate')
                 const endDateStrong = this.shadowRoot.getElementById('endDate')
-                const attStrong = this.shadowRoot.getElementById('employeeName')
+                const attStrong = this.shadowRoot.getElementById('attEmployeeName')
                 const todayDateStrong = this.shadowRoot.getElementById('todayDate')
                 employeeNameStrong.innerText=employeeName
                 startDateStrong.innerText=startDate
                 endDateStrong.innerText=endDate
                 attStrong.innerText=employeeName
                 todayDateStrong.innerText=todayDate
-                
 
-               
-                this.shadowRoot.querySelector('button').onclick = this.goToVacancyForm
+
+
+                this.shadowRoot.querySelector('button').onclick = this.goToVacancyForm 
             })
 
-        const vacancyMessage = `Caro(a) responsável 
-        Gostaria de solicitar minhas férias: 
-        Nome do Colaborador: ${employeeName} 
-        Data de inicio: ${startDate}
-        Data de fim: ${endDate}
-        Agradeço a atenção
-        Atenciosamente, ${employeeName}
-        Gerado em ${todayDate}
-        `
-        
     }
 
     goToVacancyForm() {
         const vacancyMessage = document.querySelector('vacancy-message');
-        const vacancyMessageShadowRoot = vacancyMessage && vacancyMessage.shadowRoot;
-        vacancyMessageShadowRoot.innerHTML = `<vacancy-form></vacancy-form>`
+        const parentElement = vacancyMessage.parentElement
+        parentElement.removeChild(vacancyMessage)
+        const vacancyForm = document.createElement('vacancy-form')
+        parentElement.appendChild(vacancyForm)
 
     }
 }
