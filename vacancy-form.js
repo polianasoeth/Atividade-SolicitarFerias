3977bc7feb915b8076c934528770a3a44ae78729
Poliana Soeth <polianacristinasoeth16@gmail.com>
2023-06-27 12:59:15 -0300

    Documentação JS

--- vacancy-form.js
+++ vacancy-form.js
@@ -13,6 +13,13 @@ class VacancyForm extends HTMLElement {
 
     }
 
+    /**
+     * Busca o formulário no DOM - 24 e 25
+     * Busca o valor dos campos do formulário - 26 a 28
+     * Invalida o formulário no DOM - 29 e 30
+     * Cria um elemento para renderizar a carta de férias - 31
+     * Adiciona a carta de férias no DOM - 32
+     */
     solicitarFerias() {
         const vacancyForm = document.querySelector('vacancy-form');
         const vacancyFormShadowRoot = vacancyForm && vacancyForm.shadowRoot;
@@ -21,11 +28,23 @@ class VacancyForm extends HTMLElement {
         const endDate = new Date(vacancyFormShadowRoot.getElementById('campo-data-termino').value).toLocaleDateString()
         const parentElement = vacancyForm.parentElement
         parentElement.removeChild(vacancyForm)
+        const vacancyMessage = this.createVacancyMessage(employeeName, startDate, endDate)
+        parentElement.appendChild(vacancyMessage)
+    }
+
+    /**
+     * Cria um elemento com a tag HTML vacancy-message
+     * Insere os atributos necessários para formatar a mensagem da carta de férias
+     * @param {string} employeeName Nome do colaborador
+     * @param {string} startDate Data de início 
+     * @param {string} endDate Data de término
+     */
+    createVacancyMessage(employeeName, startDate, endDate) {
         const vacancyMessage = document.createElement('vacancy-message')
         vacancyMessage.setAttribute('employeeName', employeeName)
         vacancyMessage.setAttribute('startDate', startDate)
         vacancyMessage.setAttribute('endDate', endDate)
-        parentElement.appendChild(vacancyMessage)
+        return vacancyMessage
     }
 }
 customElements.define('vacancy-form', VacancyForm)
--- vacancy-message.js
+++ vacancy-message.js
@@ -12,25 +12,17 @@ class VacancyMessage extends HTMLElement {
                 const employeeName = this.getAttribute('employeeName')
                 const startDate = this.getAttribute('startDate')
                 const endDate = this.getAttribute('endDate')
-                const todayDate = new Date().toLocaleDateString('pt-BR', { day: '2-digit', month: 'long', year: 'numeric'})
-                const employeeNameStrong = this.shadowRoot.getElementById('employeeName')
-                const startDateStrong = this.shadowRoot.getElementById('startDate')
-                const endDateStrong = this.shadowRoot.getElementById('endDate')
-                const attStrong = this.shadowRoot.getElementById('attEmployeeName')
-                const todayDateStrong = this.shadowRoot.getElementById('todayDate')
-                employeeNameStrong.innerText=employeeName
-                startDateStrong.innerText=startDate
-                endDateStrong.innerText=endDate
-                attStrong.innerText=employeeName
-                todayDateStrong.innerText=todayDate
-
-
-
-                this.shadowRoot.querySelector('button').onclick = this.goToVacancyForm 
+                this.setInnerTextInStrongElements(employeeName, startDate, endDate)
+                this.shadowRoot.querySelector('button').onclick = this.goToVacancyForm
             })
 
     }
 
+    /**
+     * Invalida a carta de férias no DOM - 27 a 29
+     * Cria o formulário - 30
+     * Adiciona o formulário no DOM - 31
+     */
     goToVacancyForm() {
         const vacancyMessage = document.querySelector('vacancy-message');
         const parentElement = vacancyMessage.parentElement
@@ -38,6 +30,27 @@ class VacancyMessage extends HTMLElement {
         const vacancyForm = document.createElement('vacancy-form')
         parentElement.appendChild(vacancyForm)
 
+    }
+
+    /**
+     * Busca os elementos onde será inserido os atributos - 44 a 48
+     * Insere o valor dos atributos como conteúdo interno desses elementos - 49 a 53
+     * @param {string} employeeName Nome do Colaborador
+     * @param {string} startDate Data de início
+     * @param {string} endDate Data de término
+     */
+    setInnerTextInStrongElements(employeeName, startDate, endDate) {
+        const todayDate = new Date().toLocaleDateString('pt-BR', { day: '2-digit', month: 'long', year: 'numeric' })
+        const employeeNameStrong = this.shadowRoot.getElementById('employeeName')
+        const startDateStrong = this.shadowRoot.getElementById('startDate')
+        const endDateStrong = this.shadowRoot.getElementById('endDate')
+        const attStrong = this.shadowRoot.getElementById('attEmployeeName')
+        const todayDateStrong = this.shadowRoot.getElementById('todayDate')
+        employeeNameStrong.innerText = employeeName
+        startDateStrong.innerText = startDate
+        endDateStrong.innerText = endDate
+        attStrong.innerText = employeeName
+        todayDateStrong.innerText = todayDate
     }
 }
 customElements.define('vacancy-message', VacancyMessage)
